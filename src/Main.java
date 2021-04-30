7548bd2f771c9ad60d489db24d3a4981b5a6058c
AbdullahAlhayek <abd1996hk@gmail.com>
2021-04-30 02:58:38 +0200

    tweede taak is uitgevoerd.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Taak een is voltooid.");
+        System.out.println("Taak twee is voltooid.");
     }
 }
